ae9e64358799a97d75de04c77623aed6df74a305
Greg Reneris <greneris@seattleu.edu>
2022-10-04 22:58:40 -0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("The 2048 game will be built, then a few moves run by an AI.");
+
+    }
+}
